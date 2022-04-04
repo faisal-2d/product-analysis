eaac5770f9defbb4c8ce5f7dab55c0de9ed757d3
faisal-2d <abdullah71faisal@gmail.com>
2022-04-04 17:34:02 +0300

    about us page updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,19 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1 className="text-3xl">This is About Page</h1>
+      <h1 className="text-3xl">Thank you for visiting our site!</h1>
+      <div>
+        <h3>Our Office</h3>
+        <address>
+          <p>Mirpur</p>
+          <p>Dhaka, Bangladesh</p>
+        </address>
+
+        <div>
+          <h3>Contact us</h3>
+          <p>+8801711223344</p>
+        </div>
+      </div>
     </div>
   );
 };
